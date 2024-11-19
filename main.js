d304a53bec9a0b391c59af0f5a26e9697a5ba6d1
Lucoria <183291283+Lucoria1@users.noreply.github.com>
2024-11-19 11:29:08 +0000

    styling button

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
         if(document.getElementById("password").value !== document.getElementById("con_pass").value){
             document.getElementById("password").style.borderColor = "red";
             document.getElementById("con_pass").style.borderColor = "red";
+            document.getElementById("pwordMatch").style.opacity = 1;
             return false
         }
     }
